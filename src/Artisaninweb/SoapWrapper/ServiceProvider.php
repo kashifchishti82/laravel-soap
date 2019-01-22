185edd2bf66a5e4f4b47378b7a38feb7dd23b4cc
kashifchsihti <kashifchishti@msn.com>
2019-01-22 12:26:30 +0000

    Apply fixes from StyleCI

--- src/Artisaninweb/SoapWrapper/ServiceProvider.php
+++ src/Artisaninweb/SoapWrapper/ServiceProvider.php
@@ -2,35 +2,35 @@
 
 namespace Artisaninweb\SoapWrapper;
 
-use \Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 
 class ServiceProvider extends LaravelServiceProvider
 {
-  /**
-   * Bootstrap the application events.
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    // Nothing here
-  }
-
-  /**
-   * Register the service provider.
-   *
-   * @return void
-   */
-  public function register()
-  {
-    $soapWrapper = new SoapWrapper();
-
-    if (is_array($this->app['config']['soapwrapper'])) {
-      $soapWrapper->addByArray($this->app['config']['soapwrapper']);
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Nothing here
     }
 
-    $this->app->bindIf(SoapWrapper::class, function () use ($soapWrapper) {
-      return $soapWrapper;
-    });
-  }
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $soapWrapper = new SoapWrapper();
+
+        if (is_array($this->app['config']['soapwrapper'])) {
+            $soapWrapper->addByArray($this->app['config']['soapwrapper']);
+        }
+
+        $this->app->bindIf(SoapWrapper::class, function () use ($soapWrapper) {
+            return $soapWrapper;
+        });
+    }
 }
--- src/Artisaninweb/SoapWrapper/SoapWrapper.php
+++ src/Artisaninweb/SoapWrapper/SoapWrapper.php
@@ -2,148 +2,150 @@
 
 namespace Artisaninweb\SoapWrapper;
 
-use Closure;
-use Artisaninweb\SoapWrapper\Exceptions\ServiceNotFound;
 use Artisaninweb\SoapWrapper\Exceptions\ServiceAlreadyExists;
 use Artisaninweb\SoapWrapper\Exceptions\ServiceMethodNotExists;
+use Artisaninweb\SoapWrapper\Exceptions\ServiceNotFound;
+use Closure;
 
 class SoapWrapper
 {
-  /**
-   * @var array
-   */
-  protected $services;
+    /**
+     * @var array
+     */
+    protected $services;
 
-  /**
-   * SoapWrapper constructor
-   */
-  public function __construct()
-  {
-    $this->services = [];
-  }
-
-  /**
-   * Add a new service to the wrapper
-   *
-   * @param string  $name
-   * @param Closure $closure
-   *
-   * @return $this
-   * @throws ServiceAlreadyExists
-   */
-  public function add($name, Closure $closure)
-  {
-    if (!$this->has($name)) {
-      $service = new Service();
-
-      $closure($service);
-
-      $this->services[$name] = $service;
-
-      return $this;
+    /**
+     * SoapWrapper constructor.
+     */
+    public function __construct()
+    {
+        $this->services = [];
     }
 
-    throw new ServiceAlreadyExists("Service '" . $name . "' already exists.");
-  }
-
-  /**
-   * Add services by array
-   *
-   * @param array $services
-   *
-   * @return $this
-   *
-   * @throws ServiceAlreadyExists
-   * @throws ServiceMethodNotExists
-   */
-  public function addByArray(array $services = [])
-  {
-    if (!empty($services)) {
-      foreach ($services as $name => $methods) {
+    /**
+     * Add a new service to the wrapper.
+     *
+     * @param string  $name
+     * @param Closure $closure
+     *
+     * @throws ServiceAlreadyExists
+     *
+     * @return $this
+     */
+    public function add($name, Closure $closure)
+    {
         if (!$this->has($name)) {
-          $service = new Service();
+            $service = new Service();
 
-          foreach ($methods as $method => $value) {
-            if (method_exists($service, $method)) {
-              $service->{$method}($value);
-            } else {
-                throw new ServiceMethodNotExists(sprintf(
+            $closure($service);
+
+            $this->services[$name] = $service;
+
+            return $this;
+        }
+
+        throw new ServiceAlreadyExists("Service '".$name."' already exists.");
+    }
+
+    /**
+     * Add services by array.
+     *
+     * @param array $services
+     *
+     * @throws ServiceAlreadyExists
+     * @throws ServiceMethodNotExists
+     *
+     * @return $this
+     */
+    public function addByArray(array $services = [])
+    {
+        if (!empty($services)) {
+            foreach ($services as $name => $methods) {
+                if (!$this->has($name)) {
+                    $service = new Service();
+
+                    foreach ($methods as $method => $value) {
+                        if (method_exists($service, $method)) {
+                            $service->{$method}($value);
+                        } else {
+                            throw new ServiceMethodNotExists(sprintf(
                   "Method '%s' does not exists on the %s service.",
                   $method,
                   $name
                 ));
-              }
-          }
+                        }
+                    }
 
-          $this->services[$name] = $service;
+                    $this->services[$name] = $service;
 
-          continue;
-        }
+                    continue;
+                }
 
-        throw new ServiceAlreadyExists(sprintf(
+                throw new ServiceAlreadyExists(sprintf(
           "Service '%s' already exists.",
           $name
         ));
-      }
+            }
+        }
+
+        return $this;
     }
 
-    return $this;
-  }
+    /**
+     * Get the client.
+     *
+     * @param string  $name
+     * @param Closure $closure
+     *
+     * @throws ServiceNotFound
+     *
+     * @return mixed
+     */
+    public function client($name, Closure $closure = null)
+    {
+        if ($this->has($name)) {
+            /** @var Service $service */
+            $service = $this->services[$name];
 
-  /**
-   * Get the client
-   *
-   * @param string  $name
-   * @param Closure $closure
-   *
-   * @return mixed
-   * @throws ServiceNotFound
-   */
-  public function client($name, Closure $closure = null)
-  {
-    if ($this->has($name)) {
-      /** @var Service $service */
-      $service = $this->services[$name];
+            if (is_null($service->getClient())) {
+                $client = new Client($service->getWsdl(), $service->getOptions());
+            } else {
+                $client = $service->getClient();
+            }
 
-      if (is_null($service->getClient())) {
-        $client = new Client($service->getWsdl(), $service->getOptions());
-      } else {
-        $client = $service->getClient();
-      }
+            return $closure($client);
+        }
 
-      return $closure($client);
+        throw new ServiceNotFound("Service '".$name."' not found.");
     }
 
-    throw new ServiceNotFound("Service '" . $name . "' not found.");
-  }
+    /**
+     * A easy access call method.
+     *
+     * @param string $call
+     * @param array  $data
+     *
+     * @return mixed
+     */
+    public function call($call, $data = [])
+    {
+        list($name, $function) = explode('.', $call, 2);
 
-  /**
-   * A easy access call method
-   *
-   * @param string $call
-   * @param array  $data
-   *
-   * @return mixed
-   */
-  public function call($call, $data = [])
-  {
-    list($name, $function) = explode('.', $call, 2);
+        return $this->client($name, function ($client) use ($function, $data) {
+            /* @var Client $client */
+            return $client->SoapCall($function, $data);
+        });
+    }
 
-    return $this->client($name, function ($client) use ($function, $data) {
-      /** @var Client $client */
-      return $client->SoapCall($function, $data);
-    });
-  }
-
-  /**
-   * Check if wrapper has service
-   *
-   * @param string $name
-   *
-   * @return bool
-   */
-  public function has($name)
-  {
-    return (array_key_exists($name, $this->services));
-  }
+    /**
+     * Check if wrapper has service.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function has($name)
+    {
+        return array_key_exists($name, $this->services);
+    }
 }
